25cff89ab6848982b24eec13569d2c9928176d0f
Hampton <me@hampton.pw>
2018-10-27 12:30:25 +0200

    Update express.js

--- _statusCheckers/express.js
+++ _statusCheckers/express.js
@@ -1,3 +1,5 @@
+// Add this code into your express app
+
 app.get('/status', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
